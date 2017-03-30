2aba64bec31e19244dcb04e256924144c4885944
Your Name <you@example.com>
2017-03-30 18:11:02 +0200

    semaine 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,43 @@
 #include <math.h>
 #include "jpg.h"
 #include "mnist.h"
-
+#include <limits.h>
 
 float distance_sq( float* V1, float* V2){
 	float d = 0;
 	for(int i=0; i<784; i++) {
-		d += (V1[i]-V2[i])*(V2[i]-V1[i]);
+		d += (V1[i]-V2[i])*(V1[i]-V2[i]);
 	}
 	return d;
  }
-
-
+float linear_classifier(float*W, float* X){
+	float d= 0;
+	for (int i=0; i<784; i++){
+			d += W[i]*X[i];
+		}
+	if (d>=0) return 1;
+	else return 0;
+}
+ 
 int main()
 {
-
+	float Error = 0;
     float** images = read_mnist("train-images.idx3-ubyte");
 	float* labels = read_labels("train-labels.idx1-ubyte");
 	float** test_images = read_mnist("t10k-images.idx3-ubyte");
-
+	float* 	test_labels = read_labels("t10k-labels.idx1-ubyte");
+	float* w=new float [784];
+	for (int i = 0; i<784; i++) w[i]=(float)rand()*2/INT_MAX-1;
+	
 	for(int i=0; i<10000; i++) {
-		printf("%u\n",i);
-		float mind = -1;
-		int NN;
-		for(int j=0; j<60000; j++) {
-			float d = distance_sq(test_images[i], images[j]);
-			if(d<mind || mind==-1){
-				mind = d;
-				NN =j;
-			}
-		}			
-		int inference = labels[NN];
-		save_jpg(test_images[i], 28, 28, "%u/%u.jpg", inference, i);
+			printf("%u\n",i);
+			
+				
+		int inference = linear_classifier (w, test_images[i]);  
+		//if(inference != (int)test_labels[i]) Error++;
+		//printf("le pourcentage d'erreur est %f %u == %u %0.2f %% \n",Error, inference, (int)test_labels[i], (Error*100)/(i+1));
+			
+		
 	}
 
 	
